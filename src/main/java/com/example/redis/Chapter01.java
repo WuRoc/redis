01d649a958882f91aa7d2ba7f69f1c3ff0bc3436
wuhepeng <1845664903@qq.com>
2022-01-18 17:34:38 +0800

    第一遍

--- src/main/java/com/example/redis/Chapter01.java
+++ src/main/java/com/example/redis/Chapter01.java
@@ -38,12 +38,14 @@ public class Chapter01 {
         System.out.println("We voted for the article, it now has votes: " + votes);
         assert Integer.parseInt(votes) > 1;
 
+        System.out.println("============================================================");
         System.out.println("The currently highest-scoring articles are:");
         List<Map<String, String>> articles = getArticles(conn, 1);
         printArticles(articles);
         assert articles.size() >= 1;
 
-        addGroups(conn, articleId, new String[]{"new-group"});
+        addGroups(conn, articleId, new String[]{"new-group","new-group-one"});
+        System.out.println("============================================================");
         System.out.println("We added the article to a new group, other articles include:");
         articles = getGroupArticles(conn, "new-group", 1);
         printArticles(articles);
@@ -63,7 +65,7 @@ public class Chapter01 {
         long now = System.currentTimeMillis() / 1000;
         //文章
         String article = "article:" + articleId;
-        //新建一个储存map直接用hmset
+        //新建一个储存map直接用 hmset
         HashMap<String, String> articleData = new HashMap<String, String>();
         articleData.put("title", title);
         articleData.put("link", link);
@@ -98,15 +100,16 @@ public class Chapter01 {
         }
     }
 
-
     public List<Map<String, String>> getArticles(Jedis conn, int page) {
         return getArticles(conn, page, "score:");
     }
 
     public List<Map<String, String>> getArticles(Jedis conn, int page, String order) {
+        //1 - 1 = 0
         int start = (page - 1) * ARTICLES_PER_PAGE;
+        //25 每页显示25条数据
         int end = start + ARTICLES_PER_PAGE - 1;
-        //排序从大到小集合
+        //排序从 大到小 集合 逆序排序
         Set<String> ids = conn.zrevrange(order, start, end);
         List<Map<String, String>> articles = new ArrayList<Map<String, String>>();
         //得到所有的id
@@ -120,9 +123,11 @@ public class Chapter01 {
     }
 
     public void addGroups(Jedis conn, String articleId, String[] toAdd) {
+        //生成article
         String article = "article:" + articleId;
-
+        //遍历all group都加入一个article
         for (String group : toAdd) {
+            //将文章增加到所属的群组
             conn.sadd("group:" + group, article);
         }
 
@@ -133,11 +138,14 @@ public class Chapter01 {
     }
 
     public List<Map<String, String>> getGroupArticles(Jedis conn, String group, int page, String order) {
+        //score:new-group
         String key = order + group;
-
+        //检查缓存结果是否已缓存 排序的结果(zset),如果没有的话就现在进行排序(交集)
         if (!conn.exists(key)) {
+            //根据评分或者发布时间对群组文章进行排序
             ZParams params = new ZParams().aggregate(ZParams.Aggregate.MAX);
             conn.zinterstore(key, params, "group:" + group, order);
+            //让redis在60秒之后自动删除这个有序集合
             conn.expire(key, 60);
         }
 
@@ -145,12 +153,18 @@ public class Chapter01 {
     }
 
     private void printArticles(List<Map<String, String>> articles) {
+
         for (Map<String, String> article : articles) {
+
             System.out.println("  id: " + article.get("id"));
+
+            //把一个map变成 一对 key value 变成一个对象,然后装进set里 进行遍历
             for (Map.Entry<String, String> entry : article.entrySet()) {
+                //上面已经打印出id 所以不再打印出来
                 if (entry.getKey().equals("id")) {
                     continue;
                 }
+
                 System.out.println("    " + entry.getKey() + ": " + entry.getValue());
             }
         }
